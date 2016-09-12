171fda729cfb028c6ac80efee9b444fe7bedc7bd
Roman <rbugakov@yandex.ru>
2016-09-12 22:49:41 +0400

    init

---
+++ src/KbIn.java
@@ -0,0 +1,1 @@
+/** * Created by Roman on 30.05.2016. */public class KbIn {    public static void main (String... args)        throws java.io.IOException    {     char ch;        System.out.println("Press any key");        do            {                ch = (char) System.in.read();                System.out.print(ch);            }        while(ch!='\r'&&ch!='\n');    }}
---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * Created by Roman on 29.05.2016.
+ */
+public class HelloWorld {
+    public static void main (String ... argc )
+        {
+          System.out.println("Hello fucken world "+argc[0]);
+            char testByte=0x50;
+            System.out.println("Тест байт = "+testByte);
+            boolean bool=1<0;
+            byte binaryData=0b00001111;
+            binaryData=(byte)(~0xA0);
+            System.out.println(binaryData);
+            System.out.println(BoolToInt(10>2));
+        }
+private static int BoolToInt(boolean bool)
+    {
+       if (bool) return 1;
+        else return 0;
+    }
+}
